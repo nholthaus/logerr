b9bf62cc75dea9dc9ee451b4e8cc25bd6a2bd938
Nic Holthaus <nicolas.holthaus@stresearch.com>
2020-12-02 14:55:12 -0500

    feat: add CoreApplication

--- qlogerr/include/coreApplication.h
+++ qlogerr/include/coreApplication.h
@@ -1,30 +1,30 @@
 //--------------------------------------------------------------------------------------------------
-// 
-//	
+//
+//  LOGERR
 //
 //--------------------------------------------------------------------------------------------------
 //
 // The MIT License (MIT)
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
-// and associated documentation files (the "Software"), to deal in the Software without 
-// restriction, including without limitation the rights to use, copy, modify, merge, publish, 
-// distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
 // Software is furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in all copies or 
+//
+// The above copyright notice and this permission notice shall be included in all copies or
 // substantial portions of the Software.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
-// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 //--------------------------------------------------------------------------------------------------
 //
 // Copyright (c) 2020 Nic Holthaus
-// 
+//
 //--------------------------------------------------------------------------------------------------
 //
 // ATTRIBUTION:
@@ -32,20 +32,20 @@
 //
 //--------------------------------------------------------------------------------------------------
 //
-/// @file	Application.h
-/// @brief	Reimplemented QApplication that can handle exceptions
+/// @file	CoreApplication.h
+/// @brief	Reimplemented QCoreApplication that can handle exceptions
 //
 //--------------------------------------------------------------------------------------------------
 
 #pragma once
-#ifndef Application_h__
-#define Application_h__
+#ifndef CoreApplication_h__
+#define CoreApplication_h__
 
 //-------------------------
 //	INCLUDES
 //-------------------------
 
-#include <QApplication> 
+#include <QCoreApplication>
 
 //-------------------------
 //	FORWARD DECLARATIONS
@@ -55,18 +55,15 @@
 //	Application
 //--------------------------------------------------------------------------------------------------
 
-class Application : public QApplication
+class CoreApplication : public QCoreApplication
 {
 	Q_OBJECT
 
 public:
+	CoreApplication(int& argc, char* argv[]);
+	~CoreApplication() override;
 
-	Application(int& argc, char* argv[]);
-	~Application() override;
-	
 	bool notify(QObject*, QEvent*) override;
-
 };
 
-
-#endif // Application_h__
+#endif    // CoreApplication_h__
---
+++ qlogerr/src/coreApplication.cpp
@@ -0,0 +1,68 @@
+//----------------------------
+//  INCLUDES
+//----------------------------
+
+#include <CoreApplication.h>
+#include <logerr>
+
+//--------------------------------------------------------------------------------------------------
+//	CoreApplication (public ) []
+//--------------------------------------------------------------------------------------------------
+CoreApplication::CoreApplication(int& argc, char* argv[])
+		: QCoreApplication(argc, argv)
+{
+
+}
+
+//--------------------------------------------------------------------------------------------------
+//	~CoreApplication (public ) []
+//--------------------------------------------------------------------------------------------------
+CoreApplication::~CoreApplication()
+{
+
+}
+
+//--------------------------------------------------------------------------------------------------
+//	notify () []
+//--------------------------------------------------------------------------------------------------
+bool CoreApplication::notify(QObject* object, QEvent* event)
+{
+	bool retVal = false;
+
+	try
+	{
+		retVal = QCoreApplication::notify(object, event);
+
+		// rethrow exceptions from threads
+		std::exception_ptr exceptionPtr = g_exceptionPtr;
+		g_exceptionPtr = nullptr;
+
+		if (exceptionPtr)
+			std::rethrow_exception(exceptionPtr);
+	}
+	catch (const logerr::exception& e)
+	{
+		if(!e.fatal())
+			LOGERR << e.what() << std::endl;
+
+		if (e.fatal())
+			throw e;
+	}
+	catch (const std::exception& e)
+	{
+		LOGERR << e.what() << std::endl;
+		throw e;
+	}
+	catch (...)
+	{
+		const char* error = "Unhandled exception caught in CoreApplication::notify() catch-all block.";
+		LOGERR << error << std::endl;
+
+		throw;
+	}
+
+	return retVal;
+}
+
+#include <moc_CoreApplication.cpp>
+ 
